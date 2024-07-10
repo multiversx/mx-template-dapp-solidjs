6ba383ed4900c8dcc13d6754c347fb34addea165
Tudor Morar <tudor.morar@multiversx.com>
2024-07-10 16:01:15 +0300

    Fetching network data working

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,19 @@ import { Layout } from "./components/Layout/Layout";
 import { useFetchData } from "hooks";
 
 export const App: Component = () => {
-  const [data] = useFetchData();
+  const { data: getNetworkData } = useFetchData();
 
   return (
     <Suspense fallback={<p>Loading...</p>}>
-      <Router>
-        {routes.map(({ path, component }) => (
-          <Route path="/" component={Layout}>
-            <Route path={path} component={component} />
-          </Route>
-        ))}
-      </Router>
+      {Boolean(getNetworkData()) && (
+        <Router>
+          {routes.map(({ path, component }) => (
+            <Route path="/" component={Layout}>
+              <Route path={path} component={component} />
+            </Route>
+          ))}
+        </Router>
+      )}
     </Suspense>
   );
 };
--- src/hooks/useInitializeNetwork.ts
+++ src/hooks/useInitializeNetwork.ts
@@ -1,4 +1,3 @@
-// useFetchData.js
 import { createResource } from "solid-js";
 import { initializeNetwork } from "@multiversx/sdk-dapp-core/out/store/slices/network/actions/initializeNetwork";
 import { EnvironmentsEnum } from "@multiversx/sdk-dapp-core/out/types";
@@ -7,11 +6,10 @@ const initNetwork = async () => {
   const data = await initializeNetwork({
     environment: EnvironmentsEnum.devnet,
   });
-  console.log({ data });
   return data;
 };
 
 export const useFetchData = () => {
   const [data, { mutate, refetch }] = createResource(initNetwork);
-  return [data, { mutate, refetch }];
+  return { data, mutate, refetch };
 };

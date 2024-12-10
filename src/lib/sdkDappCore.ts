08cc40b64a9a558a8babab8bc67777be6b63dfcd
Gavrila Andrei <andrei.gavrila@multiversx.com>
2024-12-10 15:53:58 +0200

    Update sdk-wallet-connect-provider to 5.0.1

--- src/lib/sdkDappCore.ts
+++ src/lib/sdkDappCore.ts
@@ -1,27 +1,27 @@
-export { networkSelector } from "@multiversx/sdk-dapp-core/out/store/selectors/networkSelectors";
-export { initializeNetwork } from "@multiversx/sdk-dapp-core/out/store/actions/network";
-export { getState } from "@multiversx/sdk-dapp-core/out/store/store";
-export { initApp } from "@multiversx/sdk-dapp-core/out/core/methods/initApp/initApp";
-export { trackTransactions } from "@multiversx/sdk-dapp-core/out/core/methods/trackTransactions/trackTransactions";
-export { sendTransactions } from "@multiversx/sdk-dapp-core/out/core/methods/sendTransactions/sendTransactions";
-export { getAccount } from "@multiversx/sdk-dapp-core/out/core/methods/account/getAccount";
-export { getIsLoggedIn } from "@multiversx/sdk-dapp-core/out/core/methods/account/getIsLoggedIn";
-export type { AccountType } from "@multiversx/sdk-dapp-core/out/types/account.types";
-export { getAccountProvider } from "@multiversx/sdk-dapp-core/out/core/providers/accountProvider";
-export type { SignableMessage } from "@multiversx/sdk-core/out/signableMessage";
-export type { SignedTransactionType } from "@multiversx/sdk-dapp-core/out/types/transactions.types";
+export { networkSelector } from '@multiversx/sdk-dapp-core/out/store/selectors/networkSelectors';
+export { initializeNetwork } from '@multiversx/sdk-dapp-core/out/store/actions/network';
+export { getState } from '@multiversx/sdk-dapp-core/out/store/store';
+export { initApp } from '@multiversx/sdk-dapp-core/out/core/methods/initApp/initApp';
+export { trackTransactions } from '@multiversx/sdk-dapp-core/out/core/methods/trackTransactions/trackTransactions';
+export { TransactionManager } from '@multiversx/sdk-dapp-core/out/core/managers/TransactionManager';
+export { getAccount } from '@multiversx/sdk-dapp-core/out/core/methods/account/getAccount';
+export { getIsLoggedIn } from '@multiversx/sdk-dapp-core/out/core/methods/account/getIsLoggedIn';
+export type { AccountType } from '@multiversx/sdk-dapp-core/out/types/account.types';
+export { getAccountProvider } from '@multiversx/sdk-dapp-core/out/core/providers/accountProvider';
+export type { SignableMessage } from '@multiversx/sdk-core/out/signableMessage';
+export type { SignedTransactionType } from '@multiversx/sdk-dapp-core/out/types/transactions.types';
 export {
   TRANSACTIONS_ENDPOINT,
-  ACCOUNTS_ENDPOINT,
-} from "@multiversx/sdk-dapp-core/out/apiCalls/endpoints";
+  ACCOUNTS_ENDPOINT
+} from '@multiversx/sdk-dapp-core/out/apiCalls/endpoints';
 export {
   ZERO,
   DIGITS,
-  DECIMALS,
-} from "@multiversx/sdk-dapp-core/out/constants";
-export type { IProvider } from "@multiversx/sdk-dapp-core/out/core/providers/types/providerFactory.types";
-export { ProviderTypeEnum } from "@multiversx/sdk-dapp-core/out/core/providers/types/providerFactory.types";
-export type { InitAppType } from "@multiversx/sdk-dapp-core/out/core/methods/initApp/initApp.types";
-export type { IProviderConfig } from "@multiversx/sdk-dapp-core/out/core/providers/types/providerFactory.types";
-export { createCrossWindowProvider } from "@multiversx/sdk-dapp-core/out/core/providers/helpers/crossWindow/createCrossWindowProvider";
-export { EnvironmentsEnum } from "@multiversx/sdk-dapp-core/out/types/enums.types";
+  DECIMALS
+} from '@multiversx/sdk-dapp-core/out/constants';
+export type { IProvider } from '@multiversx/sdk-dapp-core/out/core/providers/types/providerFactory.types';
+export { ProviderTypeEnum } from '@multiversx/sdk-dapp-core/out/core/providers/types/providerFactory.types';
+export type { InitAppType } from '@multiversx/sdk-dapp-core/out/core/methods/initApp/initApp.types';
+export type { IProviderConfig } from '@multiversx/sdk-dapp-core/out/core/providers/types/providerFactory.types';
+export { createCrossWindowProvider } from '@multiversx/sdk-dapp-core/out/core/providers/helpers/crossWindow/createCrossWindowProvider';
+export { EnvironmentsEnum } from '@multiversx/sdk-dapp-core/out/types/enums.types';

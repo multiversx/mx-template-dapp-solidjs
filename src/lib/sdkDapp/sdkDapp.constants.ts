0d782a443c2309205254daab4ec0a86d31a9fc8d
Tudor Morar <tudor.morar@multiversx.com>
2025-05-28 14:33:46 +0300

    Update sdk-dapp and remove unused imports

--- src/lib/sdkDapp/sdkDapp.constants.ts
+++ src/lib/sdkDapp/sdkDapp.constants.ts
@@ -3,9 +3,6 @@ export {
   TRANSACTIONS_ENDPOINT
 } from '@multiversx/sdk-dapp/out/apiCalls/endpoints';
 export {
-  EXTRA_GAS_LIMIT_GUARDED_TX,
   GAS_LIMIT,
-  GAS_PRICE,
-  VERSION
+  GAS_PRICE
 } from '@multiversx/sdk-dapp/out/constants/mvx.constants';
-export { WALLET_PROVIDER_SEND_TRANSACTION_URL } from '@multiversx/sdk-dapp/out/constants/webWalletProvider.constants';
